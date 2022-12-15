285dacaeae25936760ea01437fed7a9533fdddcd
akasharunabharathi <90937878+akasharunabharathi@users.noreply.github.com>
2022-12-14 22:01:52 -0600

    More commits

--- gardener_pi/irrigator.py
+++ gardener_pi/irrigator.py
@@ -1,2 +1,5 @@
 # What this file needs to do.
+ import moisture_detector
+
+ moisture_status = 
 
--- gardener_pi/photographer.py
+++ gardener_pi/photographer.py
@@ -4,6 +4,7 @@ from time import sleep
 
 camera = PiCamera()
 
-while(True):
-    sleep(15)
-    camera.capture('./image-data' % datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f%Z"))
+def start_camera():
+    while(True):
+        sleep(15)
+        camera.capture('./data-logs/image-data' % datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f%Z"))

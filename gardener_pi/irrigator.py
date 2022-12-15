1c2d2916d430f21575d1bf5c5a4dfdcd9e65dcf0
akasharunabharathi <90937878+akasharunabharathi@users.noreply.github.com>
2022-12-15 05:18:51 -0600

    Attempts at CompVis, Data Cleaning, Modularization, and Documentation.

--- gardener_pi/irrigator.py
+++ gardener_pi/irrigator.py
@@ -1,14 +1,20 @@
-#import libraries
-import moisture_detector
-import photographer
+# import libraries
 import time
 import csv
+import os
+import numpy as np
 import RPi.GPIO as GPIO
 
-#Sensor: Relay 1: GPIO 37
+# custom dependencies
+import moisture_detector
+import photographer
+
+# Relay Pin: GPIO 37
 relay_pin = 37
 sleep_for = 300 * 3
 data_log_address = "./data-logs/image-logs.csv"
+image_data_log_address = './data-logs/images/'
+train_test_randomizer = ["train/", "test/"]
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(relay_pin,GPIO.OUT)
@@ -22,8 +28,20 @@ try:
             
             write_to_file = open(data_log_address)
             writer = csv.writer(write_to_file)
-            contents = moisture_detector.soil_moisture_reader().append(photographer.start_camera())
+            # We're randomly picking whether we should right the image to test or to train.
+            # We want an 85%-15% split.
+            train_vs_test = np.random.choice(train_test_randomizer, 1, [0.85, 0.15])
+            image_path = os.path.join(image_data_log_address, train_vs_test + "wet_soil")
+            # The below line of code may seem complex given that it calls multiple functions and appears to be doing multiple things.
+            # We're first reading in the moisture again.
+            # There will be no significant difference in value between when we read it in the condition and when we read it here.
+            # The call to the moisture reader functions returns a list containing the class of soil wetness and analog value of wetness.
+            # The call to the photographer function returns the timestamp at which the picture was taken, which is what we're appending
+            # before writing to our CSV file.
+            contents = moisture_detector.soil_moisture_reader().append(photographer.start_camera(image_path = image_path))
+            # The parameter passed to the photographer module is the path at which we store the image
             writer.writerow(contents)
+            # The contents, of, well, "contents", have been written to the CSV file.
             write_to_file.close()
 
             # If the soil is already wet, we leave the pin closed.
@@ -34,7 +52,11 @@ try:
 
             write_to_file = open(data_log_address)
             writer = csv.writer(write_to_file)
-            contents = moisture_detector.soil_moisture_reader().append(photographer.start_camera())
+            # We're randomly picking whether we should right the image to test or to train.
+            # We want an 85%-15% split.
+            train_vs_test = np.random.choice(train_test_randomizer, 1, [0.85, 0.15])
+            image_path = os.path.join(image_data_log_address, train_vs_test + "dry_soil")
+            contents = moisture_detector.soil_moisture_reader().append(photographer.start_camera(image_path = image_path))
             writer.writerow(contents)
             write_to_file.close()
 
--- gardener_pi/photographer.py
+++ gardener_pi/photographer.py
@@ -4,7 +4,6 @@ import csv
 
 # Initial set-up
 camera = PiCamera()
-image_data_log_address = './data-logs/images/'
 
 # This function takes in no parameters.
 # When called, it does two things:
@@ -12,7 +11,7 @@ image_data_log_address = './data-logs/images/'
 # It returns the time when the image was called so that the data can be documented in conjunction 
 # with the degree of wetness from the moisture detector module.
 
-def start_camera():
+def start_camera(image_path):
     time_when_called = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%S.%f%Z")
-    camera.capture(image_data_log_address % time_when_called)
+    camera.capture(image_path)
     return time_when_called
